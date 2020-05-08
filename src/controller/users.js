7f2bb53114ec22ffc67253a050fd102368020605
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-08 12:54:27 +0700

    Update users.js

--- src/controller/users.js
+++ src/controller/users.js
@@ -1,68 +1,110 @@
-const initialData = [
-  {
-    id: 1,
-    name: 'admin',
-    email: 'admin@admin.com'
-  },
-  {
-    id: 2,
-    name: 'administrator',
-    email: 'administrator@server.com'
-  }
-]
+let { users: initialData } = require('../utils/DB')
 
 module.exports = {
   getAllUsers: (request, response) => {
     const data = {
       success: true,
       msg: 'List all users data',
-      data: {
-        pageInfo: {
-          page: 1,
-          totalPage: 10,
-          perPage: 5,
-          totalData: 50,
-          nextLink: 'http://localhost:5000/users/2',
-          prevLink: 'null'
-        }
+      data: initialData,
+      pageInfo: {
+        page: 1,
+        totalPage: 10,
+        perPage: 5,
+        totalData: 50,
+        nextLink: 'http://localhost:5000/users/2',
+        prevLink: 'null'
       }
+
     }
     response.status(200).send(data)
   },
   createUser: (request, response) => {
-    const data = {
-      success: true,
-      msg: 'user has been created',
-      data: request.body
-    }
-    response.status(201).send(data)
-  },
-  updateUser: (request, response) => {
-    // console.log(request.params.id)
-    const result = initialData.filter(function (object) {
-      return object.id === parseInt(request.params.id)
-    })
-    if (result.length > 0) {
-      const data = {
-        success: true,
-        msg: 'user has been update',
-        data: {
-          oldData: result[0],
-          newData: request.body
+    const { name, email } = request.body // mengambil data dari request.body
+    if (name && email && name !== '' && email !== '') { // name dan email itu ada atau tidak / diisit dan tidaknya
+      const isExist = initialData.filter(user => user.email === email)
+      if (isExist < 1) {
+        // data dari object akan di push aray
+        const userData = {
+          id: new Date().getTime(),
+          name,
+          email
         }
+        initialData.push(userData)
+        // mengembalikan response succes
+        const data = {
+          success: true,
+          msg: 'user has been created',
+          data: request.body
+        }
+        response.status(201).send(data)
+      } else { // jika email sudahdi gunakan
+        // akan mengembalikan response false
+        const data = {
+          success: false,
+          msg: 'Email alerdy'
+        }
+        response.status(400).send(data)
       }
-      response.status(200).send(data)
     } else {
       const data = {
         success: false,
-        msg: `user with id ${request.params.id} not found`
+        msg: 'All form must be filled'
+      }
+      response.status(400).send(data)
+    }
+  },
+  updateUser: (request, response) => {
+    // console.log(request.params.id)
+    const { id } = request.params // mendapatkan id dari parameter
+    const { name, email } = request.body // mendapatkan body
+    let idx = null
+
+    const getUser = initialData.filter(function (object, index) {
+      if (object.id === parseInt(id)) {
+        idx = index
+        return object.id === parseInt(id)
+      }
+    })
+
+    if (getUser.length > 0) {
+      if (name && email && name !== '' && email !== '') {
+        const oldData = initialData[idx] // {id : name password}
+        initialData[idx].name = name
+        const newData = {
+          name,
+          email
+        }
+        initialData[idx] = { ...oldData, ...newData }
+        const data = {
+          success: true,
+          msg: 'user has been update',
+          data: {
+            oldData,
+            newData
+          }
+        }
+        response.status(201).send(data)
+      }
+    } else {
+      const data = {
+        success: false,
+        msg: 'all form must be filled'
       }
       response.status(401).send(data)
     }
   },
   deleteUser: (request, response) => {
-    const result = initialData.filter(object => object.id === parseInt(request.params.id))
+    const { id } = request.params
+    let idx = null
+    const result = initialData.filter((o, index) => {
+      if (o.id === parseInt(id)) {
+        idx = index
+        return o.id === parseInt(id)
+      }
+    })
     if (result.length > 0) {
+      delete initialData[idx]
+      initialData = initialData.filter(obj => obj)
       const data = {
         success: true,
         msg: `user with id ${request.params.id} is deleted`
