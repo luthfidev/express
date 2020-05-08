6ca43bf663499144dc1b80926933a87532696a93
Luthfi Widya E <52591776+luthfidev@users.noreply.github.com>
2020-05-08 12:54:22 +0700

    Create DB.js

---
+++ src/utils/DB.js
@@ -0,0 +1,34 @@
+const users = [
+  {
+    id: 1,
+    name: 'admin',
+    email: 'admin@admin.com'
+  },
+  {
+    id: 2,
+    name: 'administrator',
+    email: 'administrator@server.com'
+  },
+  {
+    id: 3,
+    name: 'administrator',
+    email: 'administrator@server.com'
+  },
+  {
+    id: 4,
+    name: 'administrator',
+    email: 'administrator@server.com'
+  },
+  {
+    id: 5,
+    name: 'administrator',
+    email: 'administrator@server.com'
+  },
+  {
+    id: 6,
+    name: 'administrator',
+    email: 'administrator@server.com'
+  }
+]
+
+module.exports = { users }
